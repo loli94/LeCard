8e32e10fc72e1945ac7ab6863ece8287e6316070
Marvin Kündig <kuendigm@bluewin.ch>
2018-01-16 16:02:07 +0100

    wie alt

--- src/main.java
+++ src/main.java
@@ -3,16 +3,6 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		public void test() {
-			System.out.println("Test");
-		}
-	}
-	public void test() {
-		System.out.println("Test");
-	}
-	public void test() {
-		System.out.println("Test");
-	}
 
+	}
 }
