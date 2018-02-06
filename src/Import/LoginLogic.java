d55c4a6f22d87fc153d7986b44fc222b71c48749
larsw <larsw@DESKTOP-70TISOC>
2018-02-06 15:05:52 +0100

    c

--- src/Import/LoginLogic.java
+++ src/Import/LoginLogic.java
@@ -6,31 +6,31 @@ public class LoginLogic {
 
 public static void main(String[] args) {
 
-    String Username;
-    String Password;
+    String benutzername;
+    String passwort;
 
-    Password = "123";
-    Username = "Lars";
+    passwort = "123";
+    benutzername = "Lars";
 
     Scanner input1 = new Scanner(System.in);
-    System.out.println("Enter Username : ");
+    System.out.println("Enter Benutzername : ");
     String username = input1.next();
 
     Scanner input2 = new Scanner(System.in);
-    System.out.println("Enter Password : ");
+    System.out.println("Enter Passwort : ");
     String password = input2.next();
 
-    if (username.equals(Username) && password.equals(Password)) {
+    if (username.equals(benutzername) && password.equals(passwort)) {
 
         System.out.println("Erfolgreich angemeldet");
     }
 
-    else if (username.equals(Username)) {
+    else if (username.equals(benutzername)) {
         System.out.println("Falsches Passwort!");
-    } else if (password.equals(Password)) {
-        System.out.println("Falscher Username!");
+    } else if (password.equals(passwort)) {
+        System.out.println("Falscher Benutzername!");
     } else {
-        System.out.println("Falscher Username & Falsches Passwort!");
+        System.out.println("Falscher Benutzername & Falsches Passwort!");
     }
 
 }
