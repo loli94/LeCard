4e38828c5376f935838602b055e9d581afc3cd45
Marvin Kündig <kuendigm@bluewin.ch>
2018-02-06 15:16:21 +0100

    Erstellen des Loginframe.

--- src/LoginFrame.java
+++ src/LoginFrame.java
@@ -63,6 +63,7 @@ public class LoginFrame {
 	public void bindListener() {
 		sprachenMenu.addActionListener(new DropDownListenerSprache());
 		login.addActionListener(new ButtonListenerLogin());
+		neuerUser.addActionListener(new ButtonListenerNeuerBenutzer());
 	}
 
 	public void paint() {
@@ -131,6 +132,14 @@ public class LoginFrame {
 			Hauptfenster gui1 = new Hauptfenster(locale);
 			gui1.paint();
 			((JFrame)b.getParent().getParent().getParent().getParent().getParent()).setVisible(false);
+		}
+	}
+	
+	class ButtonListenerNeuerBenutzer implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			PanelNeuerBenutzer h1 = new PanelNeuerBenutzer(locale);
+			h1.paint();
 		}
 	}
 
